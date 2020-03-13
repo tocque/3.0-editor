c5e5de98df0f4f72dc658d01d96475b7dfd8e46c
tocque <364004564@qq.com>
2020-03-13 19:00:10 +0800

    dev data_panel

--- _server/panels/data_panel/index.js
+++ _server/panels/data_panel/index.js
@@ -1,30 +1,34 @@
 /**
  * data_panel.js 数据编辑界面
  */
-
-import commoneventTree from "./commonevent_tree.js";
+import game from "../../editor_game.js";
+import dataComment from "../../comments/data.comment.js";
 
 let components = {
-    commoneventTree
+
 }
 
 export default {
     label: "数据",
     template: /* HTML */`
-    <div class="main-side-layout" id="dataPanel">
-        <mt-side class="left" @toggle="e => leftCollapsed = e">
-            <commonevent-tree active ref="commoneventTree" @openTab="openTab"></commonevent-tree>
-        </mt-side>
-        <status-item></status-item>
+    <div id="dataPanel">
+        <mt-side-pane label="全局数据">
+            <control-list ref="main"></control-list>
+        </mt-side-pane>
+        <mt-side-pane label="初始数据">
+            <control-list ref="firstData"></control-list>
+        </mt-side-pane>
+        <mt-side-pane label="全局变量">
+            <control-list ref="values"></control-list>
+        </mt-side-pane>
+        <mt-side-pane label="全局开关">
+            <control-list ref="flags"></control-list>
+        </mt-side-pane>
     </div>
     `,
     data() {
         return {
             active: false,
-            tabs: [],
-            viewer: null,
-            tab: {},
-            leftCollapsed: false,
         }
     },
     activated() {
@@ -33,10 +37,17 @@ export default {
     deactivated() {
         this.active = false;
     },
+    mounted() {
+        this.update();
+    },
     methods: {
-        openTab(tab) {
-
-        },
+        update() {
+            const data = game.data.getTowerData();
+            console.log(this.$refs);
+            ["main", "firstData", "values", "flags"].forEach((e) => {
+                this.$refs[e].update(data[e], dataComment._data[e])
+            })
+        }
     },
     components,
 }
