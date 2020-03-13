16e4600d2656a92af9f16fd0d471c94262c1db49
tocque <364004564@qq.com>
2020-03-13 18:52:56 +0800

    despecialing airwall

--- project/maps.js
+++ project/maps.js
@@ -16,6 +16,7 @@ var maps_90f36752_8815_4be8_b32b_d7fad1d0542e =
 	"14": {"cls":"animates","id":"curseNet","noPass":false,"trigger":"passNet"},
 	"15": {"cls":"animates","id":"blueWater","noPass":true},
 	"16": {"cls":"animates","id":"water","noPass":true},
+	"17": {"cls":"terrains","id":"airwall","noPass": true},
 	"20": {"cls":"autotile","id":"037-Tree02","noPass":true},
 	"21": {"cls":"items","id":"yellowKey"},
 	"22": {"cls":"items","id":"blueKey"},
