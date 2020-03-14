55b60ab8ef1fab2a1f9bf20c9947e1143d1ea6ed
tocque <364004564@qq.com>
2020-03-14 17:53:39 +0800

    split simple editor for blockly

--- _server/editor_multi.js
+++ _server/editor_multi.js
@@ -125,3 +125,43 @@ Vue.component('code-editor', {
         }
     }
 });
+
+Vue.component('simple-editor', {
+    template: /* HTML */`
+    <div class="multi-container simple-editor" ref="container"></div>
+    `,
+    props: {
+        "lang": { default: "plaintext" },
+    },
+    data() {
+        return {
+            line: 1,
+            col: 1,
+            error: 0,
+            warn: 0,
+        }
+    },
+    async created() {
+        await monacoLoad;
+        this.multi = monaco.editor.create(this.$refs.container, {
+            theme: editor.userdata.get('monacoTheme'),
+            mouseWheelScrollSensitivity: 0.6,
+            language: this.lang,
+            automaticLayout: true,
+            tabSize: 4,
+            minimap: { enabled: false },
+            wordWrap: "on",
+        });
+    },
+    methods: {
+        setValue(value) {
+            this.multi.setValue(value);
+        },
+        getValue(value) {
+            return this.multi.getValue(value);
+        },
+        setLang(lang) {
+            monaco.editor.setModelLanguage(this.multi.getModel(), lang);
+        },
+    }
+});
